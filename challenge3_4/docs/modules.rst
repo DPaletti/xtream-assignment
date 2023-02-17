228313e79ad61f7024aae38be3e885295342754a
dpaletti <danielepaletti98@gmail.com>
2023-02-17 12:40:14 +0100

    Cleaning up gh-pages branch

--- challenge3_4/docs/modules.rst
+++
@@ -1,7 +0,0 @@
-xtream_diamonds
-===============
-
-.. toctree::
-   :maxdepth: 4
-
-   xtream_diamonds
--- challenge3_4/xtream_diamonds/challenge3/configuration.py
+++
@@ -1,4 +0,0 @@
-test_size = 0.2
-seed = 0
-target = "price"
-categorical_features = ["cut", "color", "clarity"]
